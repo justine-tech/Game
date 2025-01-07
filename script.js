6f2131999e93ef6d2496b85a241c37d2a2db949f
justine-tech <justinepoito12@gmail.com>
2025-01-08 01:07:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,42 @@
-function clickable(value){
+function clickable(value) {
     document.getElementById("result").value += value;
 }
 
-function clears(){
+function clears() {
     document.getElementById("result").value = "";
 }
 
-function equals(){
-    let total = eval(document.getElementById("result").value);
-    document.getElementById("result").value = total;
+function equals() {
+    try {
+        let total = eval(document.getElementById("result").value);
+        document.getElementById("result").value = total;
+    } catch (e) {
+        document.getElementById("result").value = "Error";
+    }
 }
 
-let clicker = 0;
+let isOn = true; // tracks whether the calculator is on or off
 
-function powers(){
-clicker ++;
-if(clicker == 1){
-    alert("OFF");
-    document.getElementById("result").value = "OFF";
-    num1.disabled = true;
-    num2.disabled = true;
-    num3.disabled = true;
-    num4.disabled = true;
-    num5.disabled = true;
-    num6.disabled = true;
-    num7.disabled = true;
-    num8.disabled = true;
-    num9.disabled = true;
-    numdot.disabled = true;
-    num0.disabled = true;
-    numeq.disabled = true;
-    numplu.disabled = true;
-    numdiv.disabled = true;
-    numtimes.disabled = true;
-    numminus.disabled = true;
- }
- else{
-    document.getElementById("result").value = "";
-    clicker--;
-    clicker--;
-    alert("ON");
-    num1.disabled = false;
-    num2.disabled = false;
-    num3.disabled = false;
-    num4.disabled = false;
-    num5.disabled = false;
-    num6.disabled = false;
-    num7.disabled = false;
-    num8.disabled = false;
-    num9.disabled = false;
-    numdot.disabled = false;
-    num0.disabled = false;
-    numeq.disabled = false;
-    numplu.disabled = false;
-    numdiv.disabled = false;
-    numtimes.disabled = false;
-    numminus.disabled = false;
- }
+function togglePower() {
+    const resultElement = document.getElementById("result");
+    const buttons = [
+        'num1', 'num2', 'num3', 'num4', 'num5',
+        'num6', 'num7', 'num8', 'num9', 'num0',
+        'numdot', 'numeq', 'numplu', 'numdiv', 
+        'numtimes', 'numminus'
+    ];
 
- 
+    if (isOn) {
+        alert("Turning OFF");
+        resultElement.value = "OFF";
+    } else {
+        alert("Turning ON");
+        resultElement.value = "";
+    }
+
+    // Toggle the button states
+    isOn = !isOn;
+    buttons.forEach(button => {
+        document.getElementById(button).disabled = !isOn;
+    });
 }
